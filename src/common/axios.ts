f65a9d8a07b0a06fddfe020a6e51f85a1979a25b
dolphin-pc <pcx474@gmail.com>
2024-07-09 12:52:18 +0900

    [FIX] mixed-content에러
    - vite.config.ts에서 proxy설정
    - axios.ts에서 baseUrl '/api' 변경

--- src/common/axios.ts
+++ src/common/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3.38.191.164/",
+  baseURL: "/api",
 });
 
 api.interceptors.response.use(
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://3.38.191.164", // 프록시할 대상 서버의 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // 요청 경로에서 '/api'를 제거
+      },
+    },
+  },
+});
